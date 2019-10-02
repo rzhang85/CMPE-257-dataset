3aacb1a8b11a3f66f11dd8c3810a232e376ec01b
Ru Zhang <rzhang85@asu.edu>
2019-10-02 16:01:54 -0700

    Load data set

---
+++ CMPE 257/load_data.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+if __name__ == '__main__':
+    print("Load Data:")
+    data = pd.read_csv("UCI_forestfires.csv")
+    ### UCI Machine Learning Forest Fires Data Set
+    # For more information, read [Cortez and Morais, 2007].
+    # 1. X - x-axis spatial coordinate within the Montesinho park map: 1 to 9
+    # 2. Y - y-axis spatial coordinate within the Montesinho park map: 2 to 9
+    # 3. month - month of the year: 'jan' to 'dec'
+    # 4. day - day of the week: 'mon' to 'sun'
+    # 5. FFMC - FFMC index from the FWI system: 18.7 to 96.20
+    # 6. DMC - DMC index from the FWI system: 1.1 to 291.3
+    # 7. DC - DC index from the FWI system: 7.9 to 860.6
+    # 8. ISI - ISI index from the FWI system: 0.0 to 56.10
+    # 9. temp - temperature in Celsius degrees: 2.2 to 33.30
+    # 10. RH - relative humidity in %: 15.0 to 100
+    # 11. wind - wind speed in km/h: 0.40 to 9.40
+    # 12. rain - outside rain in mm/m2 : 0.0 to 6.4
+    # 13. area - the burned area of the forest (in ha): 0.00 to 1090.84
+    # (this output variable is very skewed towards 0.0, thus it may make
+    # sense to model with the logarithm transform).
+
+
+    print(data)
+
+
+    his_burn_area = plt.hist(data["area"], bins="auto")
+    plt.title("Burn down area hist")
+    plt.show()
+
+
+    plt.plot(data['temp'])
+    plt.title("Temp")
+    plt.show()
+
+    plt.plot(data['RH'])
+    plt.title("Relative humidity")
+    plt.show()
+
+    plt.plot(data['wind'])
+    plt.title("Wind")
+    plt.show()
+
+    plt.plot(data['rain'])
+    plt.title("Rain")
+    plt.show()
